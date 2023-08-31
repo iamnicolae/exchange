ce57231588a300eb7a0de213d31e0d391ba27754
Nicolae <andronicnicolae15@gmail.com>
2023-08-31 12:34:55 +0300

    test update exchange and cron

--- functions/getExchange.js
+++ functions/getExchange.js
@@ -1,6 +1,6 @@
 //const fs = require('fs')
 
-const testJSON = require('./data/exchangeRates.json')
+const testJSON = require('../public/exchangeRates.json')
 
 exports.handler = async function (event, context) {
 
--- functions/updateExchange.js
+++ functions/updateExchange.js
@@ -1,10 +1,41 @@
 const fs = require('fs')
+import fetch from 'node-fetch';
+const cheerio = require('cheerio')
 
 exports.handler = async function (event, context) {
 
-  let data = JSON.stringify({ euro: "works2222" });
+  const response = await fetch('https://www.bancatransilvania.ro/conturi-si-operatiuni/operatiuni/curs-valutar/curs-valutar-la-case-de-schimb');
+  const body = await response.text();
 
-  fs.writeFile('./functions/data/exchangeRates.json', data, (err) => {
+  const $ = cheerio.load(body);
+  let rates = [];
+  const exchangeTableRows = $('#valute div.tr');
+  const currencies = ['EUR', 'CHF', 'GBP', 'USD'];
+  const exchangeRates = {
+    bank_name: 'transivlania',
+    rates: {}
+  };
+
+  exchangeTableRows.each((i, elem) => {
+    const $elem = $(elem);
+    let currency;
+
+    if (currencies.some(code => {
+      currency = code;
+
+      return $elem.text().includes(code)
+    })) {
+      exchangeRates.rates[`${currency}/RON`] = parseFloat($elem.find('div.td:nth-child(3)').text().replace(',', '.').trim());
+      exchangeRates.rates[`RON/${currency}`] = parseFloat($elem.find('div.td:nth-child(4)').text().replace(',', '.').trim());
+    }
+  });
+
+  rates.push(exchangeRates);
+
+
+  let data = JSON.stringify(rates);
+
+  fs.writeFile('./public/exchangeRates.json', data, (err) => {
     if (err) throw err;
     console.log('exchangeRates.json updated!');
   });
