4b0dd8db68ca7816125c85b267d5ebdf293cb201
Nicolae <andronicnicolae15@gmail.com>
2023-09-02 14:50:18 +0300

    fixed and cleaned netlify functions

--- functions/getExchange.js
+++ functions/getExchange.js
@@ -1,43 +1,32 @@
-//import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 const getDropboxToken = require('./utils/getDropboxToken')
-
-const getExchangeRates = require('./utils/getExchangeRates')
 
 exports.handler = async function (event, context) {
   const dropboxToken = await getDropboxToken()
   const { DROPBOX_FILE_NAME, DROPBOX_GET_FILE_URL } = process.env
 
-  const data = await getExchangeRates()
+  try {
+    const response = await fetch(DROPBOX_GET_FILE_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Authorization': `Bearer ${dropboxToken}`,
+        'Dropbox-API-Arg': `{"path": "/${DROPBOX_FILE_NAME}"}`
+      }
+    })
 
-  console.log(data)
+    const data = await response.json()
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data)
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
   }
-
-  // try {
-  //   const response = await fetch(DROPBOX_GET_FILE_URL, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/octet-stream',
-  //       'Authorization': `Bearer ${dropboxToken}`,
-  //       'Dropbox-API-Arg': `{"path": "/${DROPBOX_FILE_NAME}"}`
-  //     }
-  //   })
-
-  //   const data = await response.json()
-
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify(data)
-  //   }
-
-  // } catch (error) {
-  //   console.log(error)
-  //   return {
-  //     statusCode: 400,
-  //     body: JSON.stringify(error)
-  //   }
-  // }
 }
--- src/App.jsx
+++ src/App.jsx
@@ -12,78 +12,9 @@ function App() {
     //   .then(data => data.json())
     //   .then(msg => console.log(msg))
 
-    fetch('/.netlify/functions/getExchange')
-      .then(data => data.json())
-      .then(msg => console.log(msg))
-
-
-
-    // async function dropbox() {
-    //   var fileContent = JSON.stringify({ bank_name: '00000000000000000', });
-    //   var file = new Blob([fileContent], { type: 'application/json' });
-
-    //   try {
-    //     const data = await fetch('https://content.dropboxapi.com/2/files/upload', {
-    //       method: 'POST',
-    //       body: file,
-    //       headers: {
-    //         'Content-Type': 'application/octet-stream',
-    //         'Authorization': `Bearer sl.BlKmBAZLkwIGsfscI27KfLt9wsAR8goaJBLJFPoM0aMBvv6A847rKdB5q-Dwkh80yKsX0espIjn_ranzMTr-_rd7CY-74Mb2jx-23pCfmTZk2hd_zJXHxN8C0Rguu-_WSKRAEHzyQjn_`,
-    //         'Dropbox-API-Arg': '{"path": "/rates.json","mode": "overwrite","autorename": false,"mute": false}'
-    //       }
-    //     })
-
-    //     const res = await data.json()
-    //     console.log(res)
-
-    //   } catch (error) {
-    //     console.log(error)
-    //   }
-    // }
-
-    // dropbox();
-
-    // async function dropboxDownload() {
-    //   try {
-    //     const data = await fetch('https://content.dropboxapi.com/2/files/download', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/octet-stream',
-    //         'Authorization': `Bearer sl.BlKmBAZLkwIGsfscI27KfLt9wsAR8goaJBLJFPoM0aMBvv6A847rKdB5q-Dwkh80yKsX0espIjn_ranzMTr-_rd7CY-74Mb2jx-23pCfmTZk2hd_zJXHxN8C0Rguu-_WSKRAEHzyQjn_`,
-    //         'Dropbox-API-Arg': '{"path": "/rates.json"}'
-    //       }
-    //     })
-
-    //     const res = await data.json()
-    //     console.log(res)
-
-    //   } catch (error) {
-    //     console.log(error)
-    //   }
-    // }
-
-    // dropboxDownload();
-
-    // async function dropboxDownload() {
-    //   try {
-    //     const data = await fetch('https://www.dropbox.com/scl/fi/n3vtkj7hdsacdt6pvptx1/rates.json?rlkey=jh5ftrpfwswtyfit05dyu9llo&dl=0', {
-    //       method: 'GET',
-    //       // headers: {
-    //       //   'Content-Type': 'application/octet-stream',
-    //       //   'Authorization': `Bearer sl.BlKmBAZLkwIGsfscI27KfLt9wsAR8goaJBLJFPoM0aMBvv6A847rKdB5q-Dwkh80yKsX0espIjn_ranzMTr-_rd7CY-74Mb2jx-23pCfmTZk2hd_zJXHxN8C0Rguu-_WSKRAEHzyQjn_`,
-    //       //   'Dropbox-API-Arg': '{"path": "/rates.json"}'
-    //       // }
-    //     })
-
-    //     //const res = await data.json()
-    //     console.log(data)
-
-    //   } catch (error) {
-    //     console.log(error)
-    //   }
-    // }
-
-    // dropboxDownload();
+    // fetch('/.netlify/functions/getExchange')
+    //   .then(data => data.json())
+    //   .then(msg => console.log(msg))
 
   }, [])
 
