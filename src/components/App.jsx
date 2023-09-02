77b23494d7e4445a3c6879169ec97e7a32776440
Nicolae <andronicnicolae15@gmail.com>
2023-09-02 16:21:38 +0300

    app cleanup and fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,41 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 import Logo from './Logo'
 import Footer from './Footer'
 import Converter from './Converter'
+import Loading from './Loading'
 
 function App() {
 
+  const [exchangeRates, setExchangeRates] = useState([])
+  const [lastExchangeUpdate, setLastExchangeUpdate] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => {
-
-    // fetch('/.netlify/functions/updateExchange')
-    //   .then(data => data.json())
-    //   .then(msg => console.log(msg))
-
-    // fetch('/.netlify/functions/getExchange')
-    //   .then(data => data.json())
-    //   .then(msg => console.log(msg))
-
-  }, [])
+    fetch('/.netlify/functions/getExchange')
+      .then(res => res.json())
+      .then(data => {
+        setExchangeRates(data.exchange)
+        setLastExchangeUpdate(data.lastUpdate)
+        setIsLoading(false)
+      })
+      .catch(err => console.log(err))
+  }, []);
 
   return (
-    <div className='app'>
+    <div className="app">
       <div className="container">
-        <Logo />
-        <Footer />
-        <Converter />
+        {isLoading ?
+          <Loading />
+          :
+          <>
+            <Logo />
+            <Converter exchangeRates={exchangeRates} />
+            <Footer lastExchangeUpdate={lastExchangeUpdate} />
+          </>
+        }
       </div>
-      valutar
     </div>
   )
 }
